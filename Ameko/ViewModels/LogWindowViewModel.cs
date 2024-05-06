4f6526d752d47c44cde0c8e68a088fa960c1655d
9vult <ninevult@gmail.com>
2024-05-06 14:28:04 -0400

    Copy selected logs

--- Ameko/ViewModels/LogWindowViewModel.cs
+++ Ameko/ViewModels/LogWindowViewModel.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -17,6 +18,8 @@ namespace Ameko.ViewModels
     public class LogWindowViewModel : ViewModelBase
     {
         private List<Log> _logs;
+        public Interaction<LogWindowViewModel, string?> CopySelectedLogs { get; }
+        public ICommand CopySelectedLogsCommand { get; }
 
         public List<Log> Logs
         {
@@ -28,6 +31,11 @@ namespace Ameko.ViewModels
         {
             _logs = new List<Log>(HoloContext.Logger.Logs);
             _logs.Reverse();
+
+            CopySelectedLogs = new Interaction<LogWindowViewModel, string?>();
+            CopySelectedLogsCommand = ReactiveCommand.Create(async () => {
+                await CopySelectedLogs.Handle(this);
+            });
 
             HoloContext.Logger.PropertyChanged += Logger_LogAdded;
         }
--- Ameko/Views/LogWindow.axaml.cs
+++ Ameko/Views/LogWindow.axaml.cs
@@ -1,12 +1,47 @@
+using Ameko.ViewModels;
+using AssCS;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.ReactiveUI;
+using Holo;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
 
 namespace Ameko.Views
 {
-    public partial class LogWindow : Window
+    public partial class LogWindow : ReactiveWindow<LogWindowViewModel>
     {
+        private async Task DoCopySelectedLogsAsync(InteractionContext<LogWindowViewModel, string?> interaction)
+        {
+            var window = GetTopLevel(this);
+            var selectedLogs = logGrid.SelectedItems.Cast<Log>().Reverse().ToList();
+            if (window == null || selectedLogs == null)
+            {
+                interaction.SetOutput("");
+                return;
+            }
+            var result = string.Join(Environment.NewLine, selectedLogs);
+            await window.Clipboard!.SetTextAsync(result);
+            interaction.SetOutput(result);
+        }
+
         public LogWindow()
         {
             InitializeComponent();
+
+            this.WhenActivated((CompositeDisposable disposables) =>
+            {
+                if (ViewModel != null)
+                {
+                    ViewModel.CopySelectedLogs.RegisterHandler(DoCopySelectedLogsAsync);
+                    logGrid.KeyBindings.Clear();
+                    logGrid.KeyBindings.Add(new KeyBinding { Gesture = KeyGesture.Parse("Ctrl+C"), Command = ViewModel.CopySelectedLogsCommand });
+                }
+            });
         }
     }
 }
