8237975a063c671cbba1dce78e2385d766fece49
9vult <ninevult@gmail.com>
2024-06-05 17:54:38 -0400

    Font service
    This probably doesn't need to be its own service but leaving it as one in case opportunities for expansion arise

---
+++ Ameko/Services/FontService.cs
@@ -0,0 +1,22 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+
+namespace Ameko.Services
+{
+    internal class FontService
+    {
+        private static readonly Lazy<FontService> _instance = new Lazy<FontService>(() => new FontService());
+        public static FontService Instance => _instance.Value;
+
+        private SKFontManager _fontManager;
+
+        public IEnumerable<string> FontFamilies => _fontManager.FontFamilies;
+
+        private FontService()
+        {
+            _fontManager = SKFontManager.CreateDefault();
+        }
+        
+    }
+}
