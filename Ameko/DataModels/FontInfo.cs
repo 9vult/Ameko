088bcbd827c3dc35931acf3c803651fd0635ec70
9vult <ninevult@gmail.com>
2024-06-05 19:25:30 -0400

    Use a dedicated font library

---
+++ Ameko/DataModels/FontInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ameko.DataModels
+{
+    /// <summary>
+    /// Simplified representation of a font
+    /// </summary>
+    /// <param name="name">Name of the font family</param>
+    /// <param name="styles">List of styles in the family</param>
+    internal class FontInfo(string name, List<FontStyleMapping> styles)
+    {
+        /// <summary>
+        /// Name of the font family
+        /// </summary>
+        public string Name { get; private set; } = name;
+        /// <summary>
+        /// List of styles contained in the family
+        /// </summary>
+        public List<FontStyleMapping> Styles { get; private set; } = styles;
+    }
+
+    /// <summary>
+    /// Mapping of font styles to the file containing the style
+    /// </summary>
+    /// <param name="name">Name of the font style</param>
+    /// <param name="path">Location of the style</param>
+    internal class FontStyleMapping(string name, string? path)
+    {
+        /// <summary>
+        /// Name of the font style
+        /// </summary>
+        public string Name { get; private set; } = name;
+        /// <summary>
+        /// Location of the style
+        /// </summary>
+        public string? Path { get; private set; } = path;
+    }
+}
--- Ameko/Services/FontService.cs
+++ Ameko/Services/FontService.cs
@@ -1,22 +1,37 @@
-﻿using SkiaSharp;
+﻿using Ameko.DataModels;
+using SixLabors.Fonts;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Ameko.Services
 {
     internal class FontService
     {
-        private static readonly Lazy<FontService> _instance = new Lazy<FontService>(() => new FontService());
+        private static readonly Lazy<FontService> _instance = new(() => new FontService());
         public static FontService Instance => _instance.Value;
 
-        private SKFontManager _fontManager;
-
-        public IEnumerable<string> FontFamilies => _fontManager.FontFamilies;
+        public List<FontInfo> Fonts { get; private set; }
 
         private FontService()
         {
-            _fontManager = SKFontManager.CreateDefault();
+            Fonts = [];
+
+            IOrderedEnumerable<FontFamily> ordered = SystemFonts.Families.OrderBy(x => x.Name);
+            foreach (FontFamily family in ordered)
+            {
+                IOrderedEnumerable<FontStyle> styles = family.GetAvailableStyles().OrderBy(x => x);
+                var styleNames = new List<FontStyleMapping>();
+                foreach (FontStyle style in styles)
+                {
+                    Font font = family.CreateFont(0F, style);
+                    font.TryGetPath(out string? path);
+                    styleNames.Add(new FontStyleMapping(font.Name, path));
+                }
+                Fonts.Add(new FontInfo(family.Name, styleNames));
+            }
         }
-        
+
     }
 }
