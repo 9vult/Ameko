1f96bb11775882628972b5f3cc560a14275f873c
9vult <ninevult@gmail.com>
2024-10-05 16:53:13 -0400

    Styles and colors

---
+++ AssCS/Color.cs
@@ -0,0 +1,201 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+using System.Text.RegularExpressions;
+
+namespace AssCS;
+
+/// <summary>
+/// A color in a subtitle document, either as part of a
+/// <see cref="Style"/> or an <see cref="Overrides.Blocks.OverrideBlock"/>.
+/// </summary>
+public class Color : BindableBase
+{
+    private byte _red = 0x0;
+    private byte _green = 0x0;
+    private byte _blue = 0x0;
+    private byte _alpha = 0x0;
+
+    /// <summary>
+    /// Red component
+    /// </summary>
+    public byte Red
+    {
+        get => _red;
+        set => SetProperty(ref _red, value);
+    }
+
+    /// <summary>
+    /// Green component
+    /// </summary>
+    public byte Green
+    {
+        get => _green;
+        set => SetProperty(ref _green, value);
+    }
+
+    /// <summary>
+    /// Blue component
+    /// </summary>
+    public byte Blue
+    {
+        get => _blue;
+        set => SetProperty(ref _blue, value);
+    }
+
+    /// <summary>
+    /// Alpha component
+    /// </summary>
+    /// <remarks>0xFF is transparent, 0x00 is opaque</remarks>
+    public byte Alpha
+    {
+        get => _alpha;
+        set => SetProperty(ref _alpha, value);
+    }
+
+    /// <summary>
+    /// Luminance value of the color
+    /// </summary>
+    public double Luminance =>
+        (((0.2126 * _red) + (0.7152 * _green) + (0.0722 * _blue)) / 255) * _alpha;
+
+    /// <summary>
+    /// Ass-formatted style color string (<c>ABGR</c>)
+    /// </summary>
+    /// <returns>Ass-formatted string for styles</returns>
+    /// <remarks>For compatibility reasons, the trailing <c>&amp;</c> is dropped</remarks>
+    public string AsStyleColor()
+    {
+        return $"&H{_alpha:X2}{_blue:X2}{_green:X2}{_red:X2}";
+    }
+
+    /// <summary>
+    /// Ass-formatted override color string (<c>BGR</c>)
+    /// </summary>
+    /// <returns>Ass-formatted string</returns>
+    public string AsOverrideColor()
+    {
+        return $"&H{_blue:X2}{_green:X2}{_red:X2}&";
+    }
+
+    /// <summary>
+    /// Initialize a color from an ass-formatted string
+    /// </summary>
+    /// <param name="data">Ass-formatted string</param>
+    /// <returns>Color object represented by the string</returns>
+    /// <exception cref="ArgumentException">If the data is malformed</exception>
+    public static Color FromAss(string data)
+    {
+        var rgbRegex = @"&H([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})&?$";
+        var rgbaRegex = @"&H([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})&?$";
+        var rgbMatch = Regex.Match(data, rgbRegex);
+        var rgbaMatch = Regex.Match(data, rgbaRegex);
+        if (!rgbMatch.Success && !rgbaMatch.Success)
+            throw new ArgumentException($"Color {data} is invalid or malformed.");
+
+        if (rgbaMatch.Success)
+            return new Color
+            {
+                _alpha = Convert.ToByte(rgbaMatch.Groups[1].Value, 16),
+                _blue = Convert.ToByte(rgbaMatch.Groups[2].Value, 16),
+                _green = Convert.ToByte(rgbaMatch.Groups[3].Value, 16),
+                _red = Convert.ToByte(rgbaMatch.Groups[4].Value, 16),
+            };
+        else
+            return new Color
+            {
+                _alpha = 0x0,
+                _blue = Convert.ToByte(rgbMatch.Groups[1].Value, 16),
+                _green = Convert.ToByte(rgbMatch.Groups[2].Value, 16),
+                _red = Convert.ToByte(rgbMatch.Groups[3].Value, 16),
+            };
+    }
+
+    /// <summary>
+    /// Initialize a color from RGB values
+    /// </summary>
+    /// <param name="red">Red value</param>
+    /// <param name="green">Green value</param>
+    /// <param name="blue">Blue value</param>
+    /// <returns>Color object set to the values provided</returns>
+    /// <remarks>The color will be fully opaque</remarks>
+    public static Color FromRGB(byte red, byte green, byte blue)
+    {
+        return new Color
+        {
+            _alpha = 0x0,
+            _blue = blue,
+            _green = green,
+            _red = red,
+        };
+    }
+
+    /// <summary>
+    /// Initialize a color from RGBA values
+    /// </summary>
+    /// <param name="red">Red value</param>
+    /// <param name="green">Green value</param>
+    /// <param name="blue">Blue value</param>
+    /// <param name="alpha">Alpha value</param>
+    /// <returns>Color object set to the values provided</returns>
+    public static Color FromRGBA(byte red, byte green, byte blue, byte alpha)
+    {
+        return new Color
+        {
+            _alpha = alpha,
+            _blue = blue,
+            _green = green,
+            _red = red,
+        };
+    }
+
+    /// <summary>
+    /// Calculate the relative contrast between two colors
+    /// </summary>
+    /// <param name="a">Color A</param>
+    /// <param name="b">Color B</param>
+    /// <returns>Relative contrast</returns>
+    public static double Contrast(Color a, Color b)
+    {
+        return (a.Luminance + 0.05) / (b.Luminance + 0.05);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Color color
+            && _red == color._red
+            && _green == color._green
+            && _blue == color._blue
+            && _alpha == color._alpha;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_red, _green, _blue, _alpha);
+    }
+
+    #region Operators
+
+    public static Color operator +(Color a, Color b)
+    {
+        return new Color
+        {
+            _alpha = (byte)Math.Clamp(a.Alpha + b.Alpha, 0, 255),
+            _red = (byte)Math.Clamp(a.Red + b.Red, 0, 255),
+            _green = (byte)Math.Clamp(a.Green + b.Green, 0, 255),
+            _blue = (byte)Math.Clamp(a.Blue + b.Blue, 0, 255),
+        };
+    }
+
+    public static Color operator -(Color a, Color b)
+    {
+        return new Color
+        {
+            _alpha = (byte)Math.Clamp(a.Alpha - b.Alpha, 0, 255),
+            _red = (byte)Math.Clamp(a.Red - b.Red, 0, 255),
+            _green = (byte)Math.Clamp(a.Green - b.Green, 0, 255),
+            _blue = (byte)Math.Clamp(a.Blue - b.Blue, 0, 255),
+        };
+    }
+
+    #endregion
+}
---
+++ AssCS/Style.cs
@@ -0,0 +1,361 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+using System.Text.RegularExpressions;
+
+namespace AssCS;
+
+/// <summary>
+/// A style in a subtitle document
+/// </summary>
+/// <param name="id">ID of the style</param>
+public class Style(int id) : BindableBase
+{
+    private readonly int _id = id;
+    private string _name = "Default";
+    private string _fontFamily = "Arial";
+    private double _fontSize = 48.0d;
+    private Color _primaryColor = Color.FromRGB(0xFF, 0xFF, 0xFF);
+    private Color _secondaryColor = Color.FromRGB(0xFF, 0x0, 0x0);
+    private Color _outlineColor = Color.FromRGB(0x0, 0x0, 0x0);
+    private Color _shadowColor = Color.FromRGB(0x0, 0x0, 0x0);
+    private bool _isBold = false;
+    private bool _isItalic = false;
+    private bool _isUnderline = false;
+    private bool _isStrikethrough = false;
+    private double _scaleX = 100.0d;
+    private double _scaleY = 100.0d;
+    private double _spacing = 0.0d;
+    private double _angle = 0.0d;
+    private int _borderStyle = 1;
+    private double _borderThickness = 2.0d;
+    private double _shadowDistance = 2.0d;
+    private int _alignment = 2;
+    private Margins _margins = new(20, 20, 20);
+    private int _encoding = 1;
+
+    /// <summary>
+    /// Style ID
+    /// </summary>
+    public int Id => _id;
+
+    /// <summary>
+    /// Name of the style
+    /// </summary>
+    public string Name
+    {
+        get => _name;
+        set => SetProperty(ref _name, value);
+    }
+
+    /// <summary>
+    /// Font family used by the style
+    /// </summary>
+    public string FontFamily
+    {
+        get => _fontFamily;
+        set => SetProperty(ref _fontFamily, value);
+    }
+
+    /// <summary>
+    /// Font size used by the style
+    /// </summary>
+    public double FontSize
+    {
+        get => _fontSize;
+        set => SetProperty(ref _fontSize, value);
+    }
+
+    /// <summary>
+    /// Primary fill color
+    /// </summary>
+    public Color PrimaryColor
+    {
+        get => _primaryColor;
+        set => SetProperty(ref _primaryColor, value);
+    }
+
+    /// <summary>
+    /// Secondary fill color, used for karaoke
+    /// </summary>
+    public Color SecondaryColor
+    {
+        get => _secondaryColor;
+        set => SetProperty(ref _secondaryColor, value);
+    }
+
+    /// <summary>
+    /// Outline color
+    /// </summary>
+    public Color OutlineColor
+    {
+        get => _outlineColor;
+        set => SetProperty(ref _outlineColor, value);
+    }
+
+    /// <summary>
+    /// Shadow color
+    /// </summary>
+    public Color ShadowColor
+    {
+        get => _shadowColor;
+        set => SetProperty(ref _shadowColor, value);
+    }
+
+    /// <summary>
+    /// If the style defaults to bold
+    /// </summary>
+    public bool IsBold
+    {
+        get => _isBold;
+        set => SetProperty(ref _isBold, value);
+    }
+
+    /// <summary>
+    /// If the style defaults to italics
+    /// </summary>
+    public bool IsItalic
+    {
+        get => _isItalic;
+        set => SetProperty(ref _isItalic, value);
+    }
+
+    /// <summary>
+    /// If the style defaults to underlined
+    /// </summary>
+    public bool IsUnderline
+    {
+        get => _isUnderline;
+        set => SetProperty(ref _isUnderline, value);
+    }
+
+    /// <summary>
+    /// If the style defaults to strikethrough
+    /// </summary>
+    public bool IsStrikethrough
+    {
+        get => _isStrikethrough;
+        set => SetProperty(ref _isStrikethrough, value);
+    }
+
+    /// <summary>
+    /// Scale on the X-axis. Defaults to 100
+    /// </summary>
+    public double ScaleX
+    {
+        get => _scaleX;
+        set => SetProperty(ref _scaleX, value);
+    }
+
+    /// <summary>
+    /// Scale on the Y-axis. Defaults to 100
+    /// </summary>
+    public double ScaleY
+    {
+        get => _scaleY;
+        set => SetProperty(ref _scaleY, value);
+    }
+
+    /// <summary>
+    /// Additional spacing between characters
+    /// </summary>
+    public double Spacing
+    {
+        get => _spacing;
+        set => SetProperty(ref _spacing, value);
+    }
+
+    /// <summary>
+    /// Rotation angle
+    /// </summary>
+    public double Angle
+    {
+        get => _angle;
+        set => SetProperty(ref _angle, value);
+    }
+
+    /// <summary>
+    /// Type of border (outline) to use
+    /// </summary>
+    public int BorderStyle
+    {
+        get => _borderStyle;
+        set => SetProperty(ref _borderStyle, value);
+    }
+
+    /// <summary>
+    /// Thickness of the border (outline)
+    /// </summary>
+    public double BorderThickness
+    {
+        get => _borderThickness;
+        set => SetProperty(ref _borderThickness, value);
+    }
+
+    /// <summary>
+    /// Distance of the shadow
+    /// </summary>
+    public double ShadowDistance
+    {
+        get => _shadowDistance;
+        set => SetProperty(ref _shadowDistance, value);
+    }
+
+    /// <summary>
+    /// Alignment in the video frame
+    /// </summary>
+    /// <remarks>
+    /// Follows numpad order. Defaults to 2 (\an2),
+    /// corresponding to the bottom middle of the frame
+    /// </remarks>
+    public int Alignment
+    {
+        get => _alignment;
+        set => SetProperty(ref _alignment, value);
+    }
+
+    /// <summary>
+    /// Margins in the frame
+    /// </summary>
+    public Margins Margins
+    {
+        get => _margins;
+        set => SetProperty(ref _margins, value);
+    }
+
+    /// <summary>
+    /// Character encoding
+    /// </summary>
+    public int Encoding
+    {
+        get => _encoding;
+        set => SetProperty(ref _encoding, value);
+    }
+
+    /// <summary>
+    /// Get the ass representation of this style
+    /// </summary>
+    /// <returns>Ass-formatted string</returns>
+    public string AsAss()
+    {
+        var cleanName = _name.Replace(',', ';');
+        var cleanFont = _fontFamily.Replace(',', ';');
+        return $"Style: {cleanName},{cleanFont},{_fontSize},"
+            + $"{_primaryColor.AsStyleColor()},{_secondaryColor.AsStyleColor()},{_outlineColor.AsStyleColor()},{_shadowColor.AsStyleColor()},"
+            + $"{(_isBold ? -1 : 0)},{(_isItalic ? -1 : 0)},{(_isUnderline ? -1 : 0)},{(_isStrikethrough ? -1 : 0)},"
+            + $"{_scaleX},{_scaleY},{_spacing},{_angle},{_borderStyle},{_borderThickness},{_shadowDistance},{_alignment},"
+            + $"{_margins.Left},{_margins.Right},{_margins.Vertical},{_encoding}";
+    }
+
+    /// <summary>
+    /// Initialize a style from an ass-formatted string
+    /// </summary>
+    /// <param name="id">ID of the style</param>
+    /// <param name="data">Ass-formatted string</param>
+    /// <returns>Style object represented by the string</returns>
+    /// <exception cref="ArgumentException">If the data is malformed</exception>
+    public static Style FromAss(int id, string data)
+    {
+        var styleRegex =
+            @"Style:\ ([^,]*),([^,]*),([\d.]+),(&H[\da-fA-F]{8}&?),(&H[\da-fA-F]{8}&?),(&H[\da-fA-F]{8}&?),(&H[\da-fA-F]{8}&?),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+),(-?[\d.]+)";
+        var match = Regex.Match(data, styleRegex);
+        if (!match.Success)
+            throw new ArgumentException($"Style {data} is invalid or malformed.");
+
+        return new Style(id)
+        {
+            _name = match.Groups[1].Value,
+            _fontFamily = match.Groups[2].Value,
+            _fontSize = Convert.ToDouble(match.Groups[3].Value),
+            _primaryColor = Color.FromAss(match.Groups[4].Value),
+            _secondaryColor = Color.FromAss(match.Groups[5].Value),
+            _outlineColor = Color.FromAss(match.Groups[6].Value),
+            _shadowColor = Color.FromAss(match.Groups[7].Value),
+            _isBold = Convert.ToInt32(match.Groups[8].Value) != 0,
+            _isItalic = Convert.ToInt32(match.Groups[9].Value) != 0,
+            _isUnderline = Convert.ToInt32(match.Groups[10].Value) != 0,
+            _isStrikethrough = Convert.ToInt32(match.Groups[11].Value) != 0,
+            _scaleX = Convert.ToDouble(match.Groups[12].Value),
+            _scaleY = Convert.ToDouble(match.Groups[13].Value),
+            _spacing = Convert.ToDouble(match.Groups[14].Value),
+            _angle = Convert.ToDouble(match.Groups[15].Value),
+            _borderStyle = Convert.ToInt32(match.Groups[16].Value),
+            _borderThickness = Convert.ToDouble(match.Groups[17].Value),
+            _shadowDistance = Convert.ToDouble(match.Groups[18].Value),
+            _alignment = Convert.ToInt32(match.Groups[19].Value),
+            _margins = new Margins(
+                Convert.ToInt32(match.Groups[20].Value),
+                Convert.ToInt32(match.Groups[21].Value),
+                Convert.ToInt32(match.Groups[22].Value)
+            ),
+            _encoding = Convert.ToInt32(match.Groups[23].Value),
+        };
+    }
+
+    /// <summary>
+    /// Clone this style
+    /// </summary>
+    /// <returns>Clone of the style</returns>
+    /// <remarks>
+    /// This method currently uses serialization
+    /// as the clone method. This is subject to change.
+    /// </remarks>
+    public Style Clone()
+    {
+        return FromAss(Id, AsAss());
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Style style
+            && _id == style._id
+            && _name == style._name
+            && _fontFamily == style._fontFamily
+            && _fontSize == style._fontSize
+            && EqualityComparer<Color>.Default.Equals(_primaryColor, style._primaryColor)
+            && EqualityComparer<Color>.Default.Equals(_secondaryColor, style._secondaryColor)
+            && EqualityComparer<Color>.Default.Equals(_outlineColor, style._outlineColor)
+            && EqualityComparer<Color>.Default.Equals(_shadowColor, style._shadowColor)
+            && _isBold == style._isBold
+            && _isItalic == style._isItalic
+            && _isUnderline == style._isUnderline
+            && _isStrikethrough == style._isStrikethrough
+            && _scaleX == style._scaleX
+            && _scaleY == style._scaleY
+            && _spacing == style._spacing
+            && _angle == style._angle
+            && _borderStyle == style._borderStyle
+            && _borderThickness == style._borderThickness
+            && _shadowDistance == style._shadowDistance
+            && _alignment == style._alignment
+            && EqualityComparer<Margins>.Default.Equals(_margins, style._margins)
+            && _encoding == style._encoding;
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        hash.Add(_id);
+        hash.Add(_name);
+        hash.Add(_fontFamily);
+        hash.Add(_fontSize);
+        hash.Add(_primaryColor);
+        hash.Add(_secondaryColor);
+        hash.Add(_outlineColor);
+        hash.Add(_shadowColor);
+        hash.Add(_isBold);
+        hash.Add(_isItalic);
+        hash.Add(_isUnderline);
+        hash.Add(_isStrikethrough);
+        hash.Add(_scaleX);
+        hash.Add(_scaleY);
+        hash.Add(_spacing);
+        hash.Add(_angle);
+        hash.Add(_borderStyle);
+        hash.Add(_borderThickness);
+        hash.Add(_shadowDistance);
+        hash.Add(_alignment);
+        hash.Add(_margins);
+        hash.Add(_encoding);
+        return hash.ToHashCode();
+    }
+}
