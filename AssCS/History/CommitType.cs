d3a010d73b7ff539926890467e802a7027501d25
9vult <me@ninevolt.moe>
2024-10-07 10:14:33 -0400

    Basic history manager

---
+++ AssCS/History/CommitType.cs
@@ -0,0 +1,75 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+namespace AssCS.History;
+
+/// <summary>
+/// Type of change made in a commit
+/// </summary>
+[Flags]
+public enum CommitType
+{
+    /// <summary>
+    /// Potentially the entire file has been changed,
+    /// any saved information should be discarded
+    /// </summary>
+    New = 0,
+
+    /// <summary>
+    /// The order of events in the file have changed
+    /// </summary>
+    Order = 1 << 0,
+
+    /// <summary>
+    /// Script info has been changed
+    /// </summary>
+    ScriptInfo = 1 << 1,
+
+    /// <summary>
+    /// Styles have been changed
+    /// </summary>
+    Styles = 1 << 2,
+
+    /// <summary>
+    /// Attachments have been changed
+    /// </summary>
+    Attachment = 1 << 3,
+
+    /// <summary>
+    /// Events have been added
+    /// </summary>
+    /// <remarks>
+    EventAdd = 1 << 4,
+
+    /// <summary>
+    /// Events have been removed
+    /// </summary>
+    /// If the active event was removed, the active
+    /// line should be updated before committing this change
+    /// </remarks>
+    EventRemove = 1 << 5,
+
+    /// <summary>
+    /// Metadata fields have been changed
+    /// </summary>
+    EventMeta = 1 << 6,
+
+    /// <summary>
+    /// The start and/or end times have been changed
+    /// </summary>
+    EventTime = 1 << 7,
+
+    /// <summary>
+    /// The text has been changed
+    /// </summary>
+    EventText = 1 << 8,
+
+    /// <summary>
+    /// Everything changed!
+    /// </summary>
+    EventFull = EventMeta | EventTime | EventText,
+
+    /// <summary>
+    /// Extradata entries were changed
+    /// </summary>
+    Extradata = 1 << 9,
+}
---
+++ AssCS/History/HistoryManager.cs
@@ -0,0 +1,139 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+namespace AssCS.History;
+
+/// <summary>
+/// Manage history operations for a document
+/// </summary>
+public class HistoryManager : BindableBase
+{
+    private int _nextId = 0;
+    private readonly Stack<ICommit> _history = [];
+    private readonly Stack<ICommit> _future = [];
+
+    /// <summary>
+    /// Next commit ID
+    /// </summary>
+    private int NextId => _nextId++;
+
+    /// <summary>
+    /// If there is history to undo
+    /// </summary>
+    public bool CanUndo => _history.Count > 0;
+
+    /// <summary>
+    /// If there is future to redo
+    /// </summary>
+    public bool CanRedo => _future.Count > 0;
+
+    /// <summary>
+    /// Commit an event change to history
+    /// </summary>
+    /// <remarks>
+    /// Side-effect: Clears future stack
+    /// </remarks>
+    /// <param name="description">Short description of the change</param>
+    /// <param name="type">Type of change</param>
+    /// <param name="target">Target line</param>
+    /// <param name="parentId">ID of the parent of the target</param>
+    /// <param name="ammend">Whether to ammend the previous commit or not</param>
+    /// <returns>ID of the commit</returns>
+    /// <exception cref="InvalidOperationException">If an ammend is attempted to a non-Event commit</exception>
+    public int Commit(
+        string description,
+        CommitType type,
+        ref Event target,
+        int? parentId,
+        bool ammend = false
+    )
+    {
+        if (!ammend)
+        {
+            var id = NextId;
+            _history.Push(
+                new EventCommit
+                {
+                    Id = id,
+                    Message = description,
+                    Targets =
+                    [
+                        new EventLink
+                        {
+                            ParentId = parentId,
+                            Target = target,
+                            Type = type,
+                        },
+                    ],
+                }
+            );
+            _future.Clear();
+            NotifyAbilitiesChanged();
+            return id;
+        }
+
+        if (_history.Peek() is EventCommit commit)
+        {
+            commit.Message += ";" + description;
+            commit.Targets.Add(
+                new EventLink
+                {
+                    ParentId = parentId,
+                    Target = target,
+                    Type = type,
+                }
+            );
+            _future.Clear();
+            NotifyAbilitiesChanged();
+            return commit.Id;
+        }
+
+        throw new InvalidOperationException("Cannot ammend an Event commit to a non-Event commit");
+    }
+
+    /// <summary>
+    /// Retrieve the latest commit from the Undo stack
+    /// and add it to the Redo stack
+    /// </summary>
+    /// <remarks>
+    /// It is the duty of the calling member to act on the commit.
+    /// </remarks>
+    /// <returns>The commit</returns>
+    /// <exception cref="InvalidOperationException">If the Undo stack is empty</exception>
+    public ICommit Undo()
+    {
+        if (!CanUndo)
+            throw new InvalidOperationException("Cannot undo, no commits in the undo stack!");
+        var commit = _history.Pop();
+        _future.Push(commit);
+        NotifyAbilitiesChanged();
+        return commit;
+    }
+
+    /// <summary>
+    /// Retrieve the latest commit from the Redo stack
+    /// and add it to the Undo stack
+    /// </summary>
+    /// <remarks>
+    /// It is the duty of the calling member to act on the commit.
+    /// </remarks>
+    /// <returns>The commit</returns>
+    /// <exception cref="InvalidOperationException">If the Redo stack is empty</exception>
+    public ICommit Redo()
+    {
+        if (!CanRedo)
+            throw new InvalidOperationException("Cannot redo, no commits in the redo stack!");
+        var commit = _future.Pop();
+        _history.Push(commit);
+        NotifyAbilitiesChanged();
+        return commit;
+    }
+
+    /// <summary>
+    /// Raise property changed events for undo/redo abilities
+    /// </summary>
+    private void NotifyAbilitiesChanged()
+    {
+        RaisePropertyChanged(nameof(CanUndo));
+        RaisePropertyChanged(nameof(CanRedo));
+    }
+}
