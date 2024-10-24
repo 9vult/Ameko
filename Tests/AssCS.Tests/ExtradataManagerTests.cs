6caa2f0e9f825e2f310c5dd3169fdc498400fd50
9vult <me@ninevolt.moe>
2024-10-24 12:12:16 -0400

    Extradata test stub

---
+++ Tests/AssCS.Tests/ExtradataManagerTests.cs
@@ -0,0 +1,11 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+using FluentAssertions;
+
+namespace AssCS.Tests;
+
+public class ExtradataManagerTests
+{
+    // TODO extradata manager tests
+    // I honestly don't know enough about extradata to test this
+}
