304d69e37501190b1600a4a1b8a82a8cdf995515
9vult <me@ninevolt.moe>
2024-11-02 23:15:38 -0400

    Add and remove workspaces from a solution

--- Holo/Models/SolutionModel.cs
+++ Holo/Models/SolutionModel.cs
@@ -11,7 +11,7 @@ internal record SolutionModel
     internal const decimal CURRENT_API_VERSION = 1.0m;
 
     public required decimal Version;
-    public required List<string> ReferencedFiles;
+    public required List<string> ReferencedDocuments;
     public required List<string> Styles;
     public required int Cps;
     public required bool? UseSoftLinebreaks;
