7f1973eea3c35e39845677680efd0fa4c09d18ef
9vult <ninevult@gmail.com>
2024-11-08 10:56:38 -0500

    Start testing solution

--- Tests/Holo.Tests/SolutionTests.cs
+++ Tests/Holo.Tests/SolutionTests.cs
@@ -1,9 +1,24 @@
 ﻿// SPDX-License-Identifier: MPL-2.0
 
+using FluentAssertions;
+using Holo;
+
 namespace HoloTests;
 
 public class SolutionTests
 {
     [Fact]
-    public void Test1() { }
+    public void Save()
+    {
+        var sln = new Solution();
+        sln.StyleManager.Add(new AssCS.Style(sln.StyleManager.NextId));
+        sln.Cps = 21;
+        sln.UseSoftLinebreaks = false;
+
+        var sw = new StringWriter();
+        var isSaved = sln.Save(sw, new Uri(@"file:///c/"));
+        var result = sw.ToString();
+
+        isSaved.Should().BeTrue();
+    }
 }
