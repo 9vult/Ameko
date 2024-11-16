cf7ebe67fa885648fb33e024c4d7cbf043d4b136
9vult <ninevult@gmail.com>
2024-11-16 11:38:47 -0500

    Initial workshopping of user scripts

---
+++ Holo/Scripting/ExecutionResult.cs
@@ -0,0 +1,19 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+namespace Holo.Scripting;
+
+/// <summary>
+/// Represents the result of a script's execution
+/// </summary>
+public readonly struct ExecutionResult
+{
+    /// <summary>
+    /// The status of the script upon exit
+    /// </summary>
+    public required ExecutionStatus Status { get; init; }
+
+    /// <summary>
+    /// An optional message providing additional details about the script's execution result
+    /// </summary>
+    public string? Message { get; init; }
+}
---
+++ Holo/Scripting/LogDisplay.cs
@@ -0,0 +1,26 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+namespace Holo.Scripting;
+
+/// <summary>
+/// Specifies the behavior of the <see cref="HoloScript"/> log window
+/// </summary>
+public enum LogDisplay
+{
+    /// <summary>
+    /// The log window is displayed temporarily during script execution,
+    /// and will close automatically if the script finishes successfully
+    /// </summary>
+    Ephemeral,
+
+    /// <summary>
+    /// The log window is not displayed unless there is an error during execution
+    /// </summary>
+    OnError,
+
+    /// <summary>
+    /// The log window is displayed during script execution, and is not closed
+    /// regardless of whether the script completes successfully or not
+    /// </summary>
+    Forced,
+}
