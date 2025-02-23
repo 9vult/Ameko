36adbcadcefba6113f2486a07c0e5f76a301ec9b
9vult <me@ninevolt.moe>
2025-02-23 15:41:10 -0500

    Begin dependency control implementation

---
+++ Holo/DependencyControl/Models/Module.cs
@@ -0,0 +1,70 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+namespace Holo.DependencyControl.Models;
+
+public record Module
+{
+    /// <summary>
+    /// Name used for display purposes
+    /// </summary>
+    /// <example><c>Encode Clip</c></example>
+    public required string DisplayName { get; init; }
+
+    /// <summary>
+    /// Uniquely identifying name
+    /// </summary>
+    /// <remarks>
+    /// The most common format is <c>author.scriptName</c>, but this isn't a requirement.
+    /// Must match the <see cref="Scripting.ScriptInfo.QualifiedName"/>
+    /// defined in the module's <see cref="Scripting.ScriptInfo"/>.
+    /// </remarks>
+    /// <example><c>petzku.encodeClip</c></example>
+    public required string QualifiedName { get; init; }
+
+    /// <summary>
+    /// Short description of the module
+    /// </summary>
+    /// <example>"Encode a hardsubbed clip encompassing the current selection"</example>
+    public required string Description { get; init; }
+
+    /// <summary>
+    /// Name of the author
+    /// </summary>
+    /// <example><c>petzku</c></example>
+    public required string Author { get; init; }
+
+    /// <summary>
+    /// Current module version. (Major.Minor)
+    /// </summary>
+    /// <remarks>
+    /// Must match the <see cref="Scripting.ScriptInfo.Version"/>
+    /// defined in the module's <see cref="Scripting.ScriptInfo"/>.
+    /// </remarks>
+    public required decimal Version { get; init; }
+
+    /// <summary>
+    /// <see langword="true"/> if this is a beta module
+    /// </summary>
+    public required bool IsBetaChannel { get; init; }
+
+    /// <summary>
+    /// List of qualified module names this module depends on
+    /// </summary>
+    public required List<string> Dependencies { get; init; }
+
+    /// <summary>
+    /// List of tags for categorization
+    /// </summary>
+    public required List<string> Tags { get; init; }
+
+    /// <summary>
+    /// URL to download
+    /// </summary>
+    /// <example>https://dc.ameko.moe/scripts/9volt/9volt.example1.cs</example>
+    public required string Url { get; set; }
+
+    /// <summary>
+    /// Automatic property
+    /// </summary>
+    public string? Repository { get; set; }
+}
---
+++ Holo/DependencyControl/Models/Repository.cs
@@ -0,0 +1,98 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+using System.Text.Json;
+using NLog;
+
+namespace Holo.DependencyControl.Models;
+
+public record Repository
+{
+    /// <summary>
+    /// Name of the repository
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// Short description of the repository
+    /// </summary>
+    public required string Description { get; init; }
+
+    /// <summary>
+    /// Name of the person(s) responsible for maintaining the repository
+    /// </summary>
+    public required string Maintainer { get; init; }
+
+    /// <summary>
+    /// <see langword="true"/> if this repository is for beta modules
+    /// </summary>
+    public required bool IsBetaChannel { get; init; }
+
+    /// <summary>
+    /// Modules hosted by this repository
+    /// </summary>
+    public required List<Module> Modules { get; init; }
+
+    /// <summary>
+    /// Repositories hoisted by this repository
+    /// </summary>
+    /// <remarks>Repository json URLs</remarks>
+    public required List<string> Repositories { get; init; }
+
+    /// <summary>
+    /// Automatic property
+    /// </summary>
+    public string? Url { get; private set; }
+
+    //
+    //
+
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    /// <summary>
+    /// Build a repository
+    /// </summary>
+    /// <param name="url">Url to download repository data from</param>
+    /// <returns><see cref="Repository"/> object, or <see langword="null"/> on failure</returns>
+    public static async Task<Repository?> Build(string url)
+    {
+        using HttpClient client = new HttpClient();
+        try
+        {
+            string content = await client.GetStringAsync(url);
+            return Build(url, content);
+        }
+        catch (Exception e)
+        {
+            Logger.Error(e);
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Build a repository
+    /// </summary>
+    /// <param name="url">Url of the repository</param>
+    /// <param name="jsonContent">JSON content to parse</param>
+    /// <returns><see cref="Repository"/> object, or <see langword="null"/> on failure</returns>
+    public static Repository? Build(string url, string jsonContent)
+    {
+        try
+        {
+            var repo = JsonSerializer.Deserialize<Repository>(jsonContent);
+            if (repo is not null)
+                repo.Url = url;
+            return repo;
+        }
+        catch (JsonException e)
+        {
+            Logger.Error($"Failed to parse json from {url}");
+            Logger.Error(e);
+            return null;
+        }
+        catch (Exception e)
+        {
+            Logger.Error(e);
+            return null;
+        }
+    }
+}
