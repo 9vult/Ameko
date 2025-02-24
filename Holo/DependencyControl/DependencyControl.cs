fca171c9aa65d54b43fb78e333343f63cef9b763
9vult <ninevult@gmail.com>
2025-02-23 21:26:19 -0500

    Module management

--- Holo/DependencyControl/DependencyControl.cs
+++ Holo/DependencyControl/DependencyControl.cs
@@ -15,7 +15,9 @@ public class DependencyControl
 {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
     private static readonly JsonSerializerOptions JsonOptions = new() { IncludeFields = true };
-    private static readonly Uri ScriptRoot = new Uri(Path.Combine(Directories.DataHome, "scripts"));
+    private static readonly Uri ModulesRoot = new Uri(
+        Path.Combine(Directories.DataHome, "scripts")
+    );
 
     private Repository? _baseRepository;
     private readonly Dictionary<string, Repository> _repositoryMap;
@@ -23,8 +25,146 @@ public class DependencyControl
     private readonly ObservableCollection<Repository> _repositories;
     private readonly ObservableCollection<Module> _moduleStore;
 
+    private readonly ObservableCollection<Module> _installedModules;
+
+    /// <summary>
+    /// List of available repositories
+    /// </summary>
     public ReadOnlyObservableCollection<Repository> Repositories { get; }
+
+    /// <summary>
+    /// List of available modules
+    /// </summary>
     public ReadOnlyObservableCollection<Module> ModuleStore { get; }
+
+    /// <summary>
+    /// List of installed modules
+    /// </summary>
+    public ReadOnlyObservableCollection<Module> InstalledModules { get; }
+
+    #region Modules
+
+    public static bool IsModuleInstalled(string qualifiedName)
+    {
+        return File.Exists(ModulePath(qualifiedName));
+    }
+
+    /// <summary>
+    /// Recursively install a <see cref="Module"/> and its dependencies
+    /// </summary>
+    /// <param name="module">Module to install</param>
+    /// <returns><see cref="InstallationResult.Success"/> on success</returns>
+    public async Task<InstallationResult> InstallModule(Module module)
+    {
+        if (IsModuleInstalled(module.QualifiedName))
+            return InstallationResult.AlreadyInstalled;
+        Logger.Info($"Attempting to install module {module.QualifiedName}");
+
+        foreach (
+            var dependencyName in module
+                .Dependencies.Where(dependencyName => !IsModuleInstalled(dependencyName)) // Skip already-installed modules
+                .Where(dependencyName => dependencyName != module.QualifiedName)
+        ) // Prevent recursion
+        {
+            if (!_moduleMap.TryGetValue(dependencyName, out Module? dependency))
+            {
+                Logger.Error(
+                    $"Failed to find dependency {dependencyName} for module {module.QualifiedName}"
+                );
+                return InstallationResult.DependencyNotFound;
+            }
+
+            var depResult = await InstallModule(dependency);
+            if (depResult != InstallationResult.Success)
+                return depResult; // Cascade failures
+        }
+
+        using var client = new HttpClient();
+        try
+        {
+            await using var stream = await client.GetStreamAsync(module.Url);
+            await using var fs = new FileStream(
+                ModulePath(module.QualifiedName),
+                FileMode.OpenOrCreate
+            );
+            await stream.CopyToAsync(fs);
+            _installedModules.Add(module);
+            Logger.Info($"Successfully installed module {module.QualifiedName}");
+            return InstallationResult.Success;
+        }
+        catch (IOException e)
+        {
+            Logger.Error($"Failed to write module {module.QualifiedName} to disk");
+            Logger.Error(e);
+            return InstallationResult.FilesystemFailure;
+        }
+        catch (Exception e)
+        {
+            Logger.Error($"Failed to install module {module.QualifiedName}");
+            Logger.Error(e);
+            return InstallationResult.Failure;
+        }
+    }
+
+    /// <summary>
+    /// Uninstall a <see cref="Module"/>
+    /// </summary>
+    /// <param name="module">Module to uninstall</param>
+    /// <returns><see cref="InstallationResult.Success"/> on success</returns>
+    /// <remarks>Does not uninstall dependencies</remarks>
+    public InstallationResult UninstallModule(Module module)
+    {
+        if (!IsModuleInstalled(module.QualifiedName))
+            return InstallationResult.NotInstalled;
+        if (_installedModules.Any(m => m.Dependencies.Contains(module.QualifiedName)))
+            return InstallationResult.IsRequiredDependency;
+        Logger.Info($"Attempting to uninstall module {module.QualifiedName}");
+
+        try
+        {
+            File.Delete(ModulePath(module.QualifiedName));
+            Logger.Info($"Successfully uninstalled module {module.QualifiedName}");
+            return InstallationResult.Success;
+        }
+        catch (Exception e)
+        {
+            Logger.Error($"Failed to uninstall module {module.QualifiedName}");
+            Logger.Error(e);
+            return InstallationResult.Failure;
+        }
+    }
+
+    /// <summary>
+    /// Update a module to the latest version
+    /// </summary>
+    /// <param name="module">Module to update</param>
+    /// <returns><see cref="InstallationResult.Success"/> on success</returns>
+    public async Task<InstallationResult> UpdateModule(Module module)
+    {
+        var uninstallResult = UninstallModule(module);
+        if (uninstallResult == InstallationResult.Success)
+            return await InstallModule(module);
+        return uninstallResult;
+    }
+
+    public List<Module> GetUpdateCandidates()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Get the filepath for a module
+    /// </summary>
+    /// <param name="qualifiedName">Qualified name of the module</param>
+    /// <returns>The filepath, ending in <c>.cs</c></returns>
+    private static string ModulePath(string qualifiedName)
+    {
+        return Path.Combine(ModulesRoot.LocalPath, $"{qualifiedName}.cs");
+    }
+
+    #endregion Modules
+
+    #region Repositories
 
     /// <summary>
     /// Recursively collect repositories
@@ -54,12 +194,13 @@ public class DependencyControl
     /// </summary>
     public void GatherModules()
     {
+        _moduleMap.Clear();
         _moduleStore.Clear();
         foreach (var repo in _repositories)
         {
             foreach (var module in repo.Modules)
             {
-                module.Repository = repo.Url;
+                module.Repository = repo.Name;
                 if (_moduleMap.TryGetValue(module.QualifiedName, out Module? conflict))
                 {
                     Logger.Warn(
@@ -74,12 +215,30 @@ public class DependencyControl
     }
 
     /// <summary>
+    /// Load repositories from a list of URLs and populate the <see cref="ModuleStore"/>
+    /// </summary>
+    /// <param name="repoUrls">List of <see cref="Repository"/> URLs</param>
+    /// <remarks>Does not clear the collections</remarks>
+    public async Task BootstrapFromList(List<string> repoUrls)
+    {
+        foreach (var url in repoUrls)
+        {
+            var repo = await Repository.Build(url);
+            if (repo is not null)
+                await GatherRepositories(repo);
+        }
+        GatherModules();
+    }
+
+    /// <summary>
     /// Set up the base repository
     /// </summary>
     private async Task SetUpBaseRepository()
     {
         _baseRepository = await Repository.Build("");
     }
+
+    #endregion Repositories
 
     public DependencyControl()
     {
@@ -88,8 +247,12 @@ public class DependencyControl
         _repositories = [];
         _moduleStore = [];
 
+        _installedModules = [];
+
         Repositories = new ReadOnlyObservableCollection<Repository>(_repositories);
         ModuleStore = new ReadOnlyObservableCollection<Module>(_moduleStore);
+
+        InstalledModules = new ReadOnlyObservableCollection<Module>(_installedModules);
 
         _ = SetUpBaseRepository();
     }
