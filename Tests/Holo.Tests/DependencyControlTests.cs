d93e82f6bf1cc5f7aae64875160d8129d6862176
9vult <me@ninevolt.moe>
2025-02-27 12:52:57 -0500

    First mocked filesystem tests
    Will need to expand this strategy to other filesystem-interacting classes

---
+++ Tests/Holo.Tests/DependencyControlTests.cs
@@ -0,0 +1,35 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+using System.IO.Abstractions.TestingHelpers;
+using Holo.Scripting;
+using Shouldly;
+
+namespace Holo.Tests;
+
+public class DependencyControlTests
+{
+    [Fact]
+    public void IsModuleInstalled_True()
+    {
+        var fileSystem = new MockFileSystem(
+            new Dictionary<string, MockFileData>
+            {
+                { DependencyControl.ModulePath("author.test"), new MockFileData(string.Empty) },
+            }
+        );
+
+        var dc = new DependencyControl(fileSystem);
+
+        dc.IsModuleInstalled("author.test").ShouldBeTrue();
+    }
+
+    [Fact]
+    public void IsModuleInstalled_False()
+    {
+        var fileSystem = new MockFileSystem();
+
+        var dc = new DependencyControl(fileSystem);
+
+        dc.IsModuleInstalled("author.test").ShouldBeFalse();
+    }
+}
