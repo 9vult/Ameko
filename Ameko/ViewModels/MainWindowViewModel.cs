893d28d2df748d83d3279119cc029af0eb147e2a
9vult <me@ninevolt.moe>
2025-03-28 11:58:27 -0400

    Add Ameko project to solution

---
+++ Ameko/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿// SPDX-License-Identifier: GPL-3.0-only
+
+using Ameko.Services;
+
+namespace Ameko.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = $"Welcome to Ameko {VersionService.FullLabel}!";
+}
---
+++ Ameko/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿// SPDX-License-Identifier: GPL-3.0-only
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Ameko.ViewModels;
+
+public class ViewModelBase : ObservableObject { }
