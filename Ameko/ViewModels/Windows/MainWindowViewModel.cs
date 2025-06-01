e5ec203f934ee4cb9781d303136df34811a10f32
9vult <me@ninevolt.moe>
2025-05-31 22:00:42 -0400

    Add a placeholder Solution Explorer

--- Ameko/ViewModels/Windows/MainWindowViewModel.cs
+++ Ameko/ViewModels/Windows/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Reactive;
 using System.Reactive.Linq;
 using System.Windows.Input;
 using Ameko.Services;
+using Holo;
 using ReactiveUI;
 
 namespace Ameko.ViewModels.Windows;
@@ -11,30 +12,33 @@ namespace Ameko.ViewModels.Windows;
 public partial class MainWindowViewModel : ViewModelBase
 {
     #region Interactions
-
     public Interaction<LogWindowViewModel, Unit> ShowLogWindow { get; }
-
     #endregion
+
     #region Commands
-
     public ICommand ShowLogWindowCommand { get; }
-
     #endregion
+
+    public Solution Solution
+    {
+        get => HoloContext.Instance.Solution;
+        set
+        {
+            HoloContext.Instance.Solution = value;
+            this.RaisePropertyChanged();
+        }
+    }
 
     public string Greeting { get; } = $"Welcome to Ameko {VersionService.FullLabel}!";
 
     public MainWindowViewModel()
     {
         #region Interactions
-
         ShowLogWindow = new Interaction<LogWindowViewModel, Unit>();
-
         #endregion
 
         #region Commands
-
         ShowLogWindowCommand = CreateShowLogWindowCommand();
-
         #endregion
     }
 }
