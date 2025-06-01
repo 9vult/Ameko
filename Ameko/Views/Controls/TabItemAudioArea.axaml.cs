67d2ade99c9786936c8d022c60cf67c71fe0a594
9vult <48932637+9vult@users.noreply.github.com>
2025-06-01 12:56:34 -0400

    Tab item segments

---
+++ Ameko/Views/Controls/TabItemAudioArea.axaml.cs
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ameko.Views.Controls;
+
+public partial class TabItemAudioArea : UserControl
+{
+    public TabItemAudioArea()
+    {
+        InitializeComponent();
+    }
+}
