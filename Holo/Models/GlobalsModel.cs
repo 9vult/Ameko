8e2e7d2868c2d25e04da47c404913a8802c535c3
9vult <me@ninevolt.moe>
2025-06-07 00:38:43 -0400

    Create globals manager

---
+++ Holo/Models/GlobalsModel.cs
@@ -0,0 +1,19 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+using System.Text.Json.Serialization;
+
+namespace Holo.Models;
+
+/// <summary>
+/// Simplified representation of a <see cref="Globals"/> for
+/// serialization purposes
+/// </summary>
+internal record GlobalsModel
+{
+    [JsonIgnore]
+    internal const double CurrentApiVersion = 1.0d;
+
+    public required double Version;
+    public required string[] Styles;
+    public required string[] Colors;
+}
