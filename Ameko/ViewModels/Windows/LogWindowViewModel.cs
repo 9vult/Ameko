6538fcf0a6c9f75e6ffafd0b4f815e0c5d39379f
9vult <48932637+9vult@users.noreply.github.com>
2025-06-08 16:56:51 -0400

    Auto-scroll logs

--- Ameko/ViewModels/Windows/LogWindowViewModel.cs
+++ Ameko/ViewModels/Windows/LogWindowViewModel.cs
@@ -2,11 +2,29 @@
 
 using System.Collections.ObjectModel;
 using Holo;
+using ReactiveUI;
 
 namespace Ameko.ViewModels.Windows;
 
 public partial class LogWindowViewModel : ViewModelBase
 {
+    private object? _selectedLog;
+
+    public object? SelectedLog
+    {
+        get => _selectedLog;
+        set => this.RaiseAndSetIfChanged(ref _selectedLog, value);
+    }
+
     public static ReadOnlyObservableCollection<string> LogEntries =>
         HoloContext.Instance.LogEntries;
+
+    public LogWindowViewModel()
+    {
+        HoloContext.Instance.LogEntries.CollectionChanged += (_, args) =>
+        {
+            if (args.NewItems is not null && args.NewItems.Count > 0)
+                SelectedLog = args.NewItems[^1];
+        };
+    }
 }
