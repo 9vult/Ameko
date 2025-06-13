fa534fb9cd4c6543a7ae3894b91f61a2cd3781ae
9vult <48932637+9vult@users.noreply.github.com>
2025-06-13 08:49:59 -0400

    Implement Keybind Registrar

--- Holo/Configuration/IKeybindRegistrar.cs
+++ Holo/Configuration/IKeybindRegistrar.cs
@@ -9,38 +9,74 @@ namespace Holo.Configuration;
 public interface IKeybindRegistrar
 {
     /// <summary>
-    /// Registers a new keybind or replaces an existing one with the same qualified name
+    /// Registers a new keybind or replaces an existing one with the same name
     /// </summary>
     /// <param name="keybind">The <see cref="Keybind"/> to register</param>
-    void RegisterKeybind(Keybind keybind);
+    bool RegisterKeybind(Keybind keybind);
 
     /// <summary>
-    /// Removes the keybind associated with the specified keybind name
+    /// Registers new keybinds or replaces existing ones with the same name
+    /// </summary>
+    /// <param name="keybinds">The <see cref="Keybind"/>s to register</param>
+    bool RegisterKeybinds(IList<Keybind> keybinds);
+
+    /// <summary>
+    /// Removes the keybind associated with the specified name
     /// </summary>
     /// <param name="qualifiedName">The keybind name</param>
-    void DeregisterKeybind(string qualifiedName);
+    bool DeregisterKeybind(string qualifiedName);
 
     /// <summary>
-    /// Retrieves the keybind registered under the specified qualified name.
+    /// Apply an override on an existing keybind
     /// </summary>
-    /// <param name="qualifiedName">The action name to look up</param>
+    /// <param name="qualifiedName">Name of the keybind to override</param>
+    /// <param name="keybind">Keybind to override the default with</param>
+    /// <param name="context">Contexts to override the default with</param>
+    /// <returns><see langword="true"/> if successful</returns>
+    bool ApplyOverride(string qualifiedName, string? keybind, KeybindContext? context);
+
+    /// <summary>
+    /// Clears overrides from a keybind
+    /// </summary>
+    /// <param name="qualifiedName">Name of the keybind to clear overrides from</param>
+    /// <returns><see langword="true"/> if successful</returns>
+    bool ClearOverride(string qualifiedName);
+
+    /// <summary>
+    /// Retrieves the keybind registered under the specified name.
+    /// </summary>
+    /// <param name="qualifiedName">The keybind name to look up</param>
     /// <returns>The associated <see cref="Keybind"/>, or <see langword="null" /> if not found</returns>
     Keybind? GetKeybind(string qualifiedName);
 
     /// <summary>
     /// Attempts to retrieve the keybind registered under the specified keybind name
     /// </summary>
-    /// <param name="qualifiedName">The action name to look up</param>
+    /// <param name="qualifiedName">The keybind name to look up</param>
     /// <param name="keybind">The keybind, if found, otherwise, <see langword="null" /></param>
     /// <returns><see langword="true" /> if the keybind was found</returns>
-    bool TryGetKeybind(string qualifiedName, out Keybind keybind);
+    bool TryGetKeybind(string qualifiedName, out Keybind? keybind);
 
     /// <summary>
-    /// Determines whether a keybind is registered under the specified qualified name.
+    /// Determines whether a keybind is registered under the specified name.
     /// </summary>
     /// <param name="qualifiedName">The keybind name to check</param>
     /// <returns><see langword="true" /> if a keybind is registered to the given name</returns>
     bool IsKeybindRegistered(string qualifiedName);
+
+    /// <summary>
+    /// Enable a keybind
+    /// </summary>
+    /// <param name="qualifiedName">The name of the keybind to enable</param>
+    /// <returns><see langword="true"/> if successful</returns>
+    bool EnableKeybind(string qualifiedName);
+
+    /// <summary>
+    /// Disable a keybind
+    /// </summary>
+    /// <param name="qualifiedName">The name of the keybind to disable</param>
+    /// <returns><see langword="true"/> if successful</returns>
+    bool DisableKeybind(string qualifiedName);
 
     /// <summary>
     /// Retrieves all keybinds that match the specified context filter
@@ -48,4 +84,17 @@ public interface IKeybindRegistrar
     /// <param name="filter">A combination of <see cref="KeybindContext"/> flags used to filter keybinds</param>
     /// <returns>Keybinds that are active in any of the specified contexts</returns>
     IEnumerable<Keybind> GetKeybinds(KeybindContext filter);
+
+    /// <summary>
+    /// Retrieves all keybinds that have been overridden
+    /// </summary>
+    /// <returns>Keybinds that have been overridden</returns>
+    IEnumerable<Keybind> GetOverridenKeybinds();
+
+    /// <summary>
+    /// Event Handler for <see cref="IKeybindRegistrar.OnKeybindsChanged"/>
+    /// </summary>
+    public delegate void KeybindsChangedEventHandler(object sender, EventArgs e);
+
+    event KeybindsChangedEventHandler? OnKeybindsChanged;
 }
