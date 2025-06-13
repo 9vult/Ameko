2be96dbca2790ffe830e76040583b7baaeebfbe1
9vult <me@ninevolt.moe>
2025-06-13 15:40:04 -0400

    Add additional KeybindTarget constructors to reduce boilerplate

--- Holo/Configuration/Keybinds/KeybindTargetAttribute.cs
+++ Holo/Configuration/Keybinds/KeybindTargetAttribute.cs
@@ -9,20 +9,43 @@ public sealed class KeybindTargetAttribute : Attribute
     public string? DefaultKey { get; }
     public KeybindContext? DefaultContext { get; }
 
-    public KeybindTargetAttribute(string qualifiedName, string? defaultKey = null)
+    /// <summary>
+    /// Denotes a keybind target with a default keybind assigned
+    /// </summary>
+    /// <param name="qualifiedName">Keybind identifier</param>
+    /// <param name="defaultKey">Default keybind</param>
+    /// <remarks>This constructor uses the class-level context, if applicable</remarks>
+    public KeybindTargetAttribute(string qualifiedName, string defaultKey)
     {
         QualifiedName = qualifiedName;
         DefaultKey = defaultKey;
     }
 
+    /// <summary>
+    /// Denotes a keybind target with a default keybind assigned
+    /// </summary>
+    /// <param name="qualifiedName">Keybind identifier</param>
+    /// <param name="defaultKey">Default keybind</param>
+    /// <param name="defaultContext">Default keybind context</param>
     public KeybindTargetAttribute(
         string qualifiedName,
-        string? defaultKey,
+        string defaultKey,
         KeybindContext defaultContext
     )
     {
         QualifiedName = qualifiedName;
         DefaultKey = defaultKey;
         DefaultContext = defaultContext;
+    }
+
+    /// <summary>
+    /// Denotes a keybind target without a default keybind assigned
+    /// </summary>
+    /// <param name="qualifiedName">Keybind identifier</param>
+    /// <remarks>This constructor uses the class-level context, if applicable</remarks>
+    public KeybindTargetAttribute(string qualifiedName)
+    {
+        QualifiedName = qualifiedName;
+        DefaultKey = null;
     }
 }
