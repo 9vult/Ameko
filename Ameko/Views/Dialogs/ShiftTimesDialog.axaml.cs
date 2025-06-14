4337b774c7b5044b673009e1d6d5ff7c91ce1dc9
9vult <me@ninevolt.moe>
2025-06-13 22:38:04 -0400

    Designing shift times dialog

---
+++ Ameko/Views/Dialogs/ShiftTimesDialog.axaml.cs
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ameko.Views.Dialogs;
+
+public partial class ShiftTimesDialog : Window
+{
+    public ShiftTimesDialog()
+    {
+        InitializeComponent();
+    }
+}
