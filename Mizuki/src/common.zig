603911f30353392c73cd82e8eaeba512e4069c8d
9vult <me@ninevolt.moe>
2025-06-14 18:20:08 -0400

    Initial setup of Mizuki project: Print FFMS version

---
+++ Mizuki/src/common.zig
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MPL-2.0
+
+/// Version of a backing library
+pub const BackingVersion = extern struct {
+    major: c_int,
+    minor: c_int,
+    patch: c_int,
+};
+
+/// A frame of a video
+pub const VideoFrame = extern struct {
+    isKeyFrame: bool,
+    width: c_int,
+    height: c_int,
+    data: [4][*c]const u8,
+    lineSize: [4]c_int,
+};
