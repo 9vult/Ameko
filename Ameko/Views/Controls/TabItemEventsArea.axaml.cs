8b16a0fe32467435623babd6f325e1e8c35aa1ed
9vult <me@ninevolt.moe>
2025-06-15 18:59:39 -0400

    Note what's happening in the selection changed handler

--- Ameko/Views/Controls/TabItemEventsArea.axaml.cs
+++ Ameko/Views/Controls/TabItemEventsArea.axaml.cs
@@ -20,8 +20,11 @@ public partial class TabItemEventsArea : ReactiveUserControl<TabItemViewModel>
     {
         var active = (Event)EventsGrid.SelectedItem;
         var selection = EventsGrid.SelectedItems.Cast<Event>().ToList();
+        // Side effect: set SelectionChanging to true to prevent Commit() calls
         ViewModel?.Workspace.SelectionManager.Select(active, selection);
 
+        // Mark the selection change complete (on the UI thread)
+        // to re-allow Commit() calls
         Dispatcher.UIThread.Post(
             () =>
             {
