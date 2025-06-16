32272405513310b7b0ce117946ed8744eaeb730e
9vult <me@ninevolt.moe>
2025-06-15 22:59:06 -0400

    Add a toolbox area for the git service

---
+++ Ameko/Views/Controls/GitToolbox.axaml.cs
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ameko.Views.Controls;
+
+public partial class GitToolbox : UserControl
+{
+    public GitToolbox()
+    {
+        InitializeComponent();
+    }
+}
