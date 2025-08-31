d0634c63089c221b51e07dbc44bb3a66b3ec19ae
9vult <me@ninevolt.moe>
2025-08-31 09:34:20 -0400

    Undo/Redo tests

--- Tests/Holo.Tests/WorkspaceTests.cs
+++ Tests/Holo.Tests/WorkspaceTests.cs
@@ -106,4 +106,158 @@ public class WorkspaceTests
         wsp.SelectionManager.ActiveEvent.ShouldBe(e2);
         wsp.SelectionManager.SelectedEventCollection.Count.ShouldBe(2);
     }
+
+    [Fact]
+    public void Undo_AddEvent()
+    {
+        var wsp = new Workspace(new Document(true), 1);
+        var e1 = new Event(99);
+        var e2 = new Event(100);
+
+        wsp.Document.EventManager.AddLast(e1);
+        wsp.Commit(e1, ChangeType.Add);
+        wsp.Document.EventManager.AddLast(e2);
+        wsp.Commit(e2, ChangeType.Add);
+
+        wsp.Document.HistoryManager.CanUndo.ShouldBeTrue();
+        wsp.Undo();
+        wsp.Document.EventManager.Events.ShouldNotContain(e2);
+        wsp.Document.EventManager.Events.ShouldContain(e1);
+
+        wsp.Document.HistoryManager.CanRedo.ShouldBeTrue();
+        wsp.Undo();
+        wsp.Document.EventManager.Events.ShouldNotContain(e2);
+        wsp.Document.EventManager.Events.ShouldNotContain(e1);
+    }
+
+    [Fact]
+    public void Redo_AddEvent()
+    {
+        var wsp = new Workspace(new Document(true), 1);
+        var e1 = new Event(99);
+        var e2 = new Event(100);
+
+        wsp.Document.EventManager.AddLast(e1);
+        wsp.Commit(e1, ChangeType.Add);
+        wsp.Document.EventManager.AddLast(e2);
+        wsp.Commit(e2, ChangeType.Add);
+        wsp.Undo();
+        wsp.Undo();
+
+        wsp.Document.EventManager.Events.ShouldNotContain(e2);
+        wsp.Document.EventManager.Events.ShouldNotContain(e1);
+        wsp.Document.HistoryManager.CanUndo.ShouldBeFalse();
+        wsp.Document.HistoryManager.CanRedo.ShouldBeTrue();
+
+        wsp.Redo();
+        wsp.Document.EventManager.Events.ShouldContain(e1);
+        wsp.Document.EventManager.Events.ShouldNotContain(e2);
+
+        wsp.Redo();
+        wsp.Document.EventManager.Events.ShouldContain(e1);
+        wsp.Document.EventManager.Events.ShouldContain(e2);
+
+        wsp.Document.HistoryManager.CanUndo.ShouldBeTrue();
+        wsp.Document.HistoryManager.CanRedo.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void Undo_RemoveEvent()
+    {
+        var wsp = new Workspace(new Document(true), 1);
+        var e1 = new Event(99);
+        wsp.Document.EventManager.AddLast(e1);
+
+        wsp.Document.EventManager.Remove(e1.Id);
+        wsp.Commit(e1, ChangeType.Remove);
+
+        wsp.Document.EventManager.Events.ShouldNotContain(e1);
+        wsp.Document.HistoryManager.CanUndo.ShouldBeTrue();
+
+        wsp.Undo();
+        wsp.Document.EventManager.Events.ShouldContain(e1);
+    }
+
+    [Fact]
+    public void Redo_RemoveEvent()
+    {
+        var wsp = new Workspace(new Document(true), 1);
+        var e1 = new Event(99);
+        wsp.Document.EventManager.AddLast(e1);
+
+        wsp.Document.EventManager.Remove(e1.Id);
+        wsp.Commit(e1, ChangeType.Remove);
+        wsp.Undo();
+        wsp.Document.EventManager.Events.ShouldContain(e1);
+        wsp.Document.HistoryManager.CanRedo.ShouldBeTrue();
+
+        wsp.Redo();
+        wsp.Document.EventManager.Events.ShouldNotContain(e1);
+    }
+
+    [Fact]
+    public void Undo_ModifyEvent_Once()
+    {
+        var wsp = new Workspace(new Document(true), 1);
+        var e1 = wsp.Document.EventManager.Head;
+
+        wsp.Document.HistoryManager.BeginTransaction([e1]);
+        e1.Text = "Hello!";
+        wsp.Commit(e1, ChangeType.Modify);
+
+        wsp.Undo();
+        wsp.Document.EventManager.Head.Text.ShouldBe(string.Empty);
+    }
+
+    [Fact]
+    public void Redo_ModifyEvent_Once()
+    {
+        var wsp = new Workspace(new Document(true), 1);
+        var e1 = wsp.Document.EventManager.Head;
+
+        wsp.Document.HistoryManager.BeginTransaction([e1]);
+        e1.Text = "Hello!";
+        wsp.Commit(e1, ChangeType.Modify);
+        wsp.Undo();
+
+        wsp.Document.HistoryManager.CanRedo.ShouldBeTrue();
+        wsp.Redo();
+
+        wsp.Document.EventManager.Head.Text.ShouldBe("Hello!");
+    }
+
+    [Fact]
+    public void Undo_ModifyEvent_Multiple()
+    {
+        var wsp = new Workspace(new Document(true), 1);
+        var e1 = wsp.Document.EventManager.Head;
+
+        wsp.Document.HistoryManager.BeginTransaction([e1]);
+        e1.Text = "Hello!";
+        wsp.Commit(e1, ChangeType.Modify);
+        e1.Text = "Hello! World!";
+        wsp.Commit(e1, ChangeType.Modify);
+
+        wsp.Undo();
+        wsp.Document.EventManager.Head.Text.ShouldBe(string.Empty);
+    }
+
+    [Fact]
+    public void Redo_ModifyEvent_Multiple()
+    {
+        var wsp = new Workspace(new Document(true), 1);
+        var e1 = wsp.Document.EventManager.Head;
+
+        wsp.Document.HistoryManager.BeginTransaction([e1]);
+        e1.Text = "Hello!";
+        wsp.Commit(e1, ChangeType.Modify);
+        e1.Text = "Hello! World!";
+        wsp.Commit(e1, ChangeType.Modify);
+        wsp.Undo();
+
+        wsp.Document.HistoryManager.CanRedo.ShouldBeTrue();
+        wsp.Redo();
+
+        wsp.Document.EventManager.Head.Text.ShouldBe("Hello! World!");
+    }
 }
