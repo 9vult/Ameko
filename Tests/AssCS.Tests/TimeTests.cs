f757a02fc18ec03a1d4eb06eafcd00ffbc3e6835
9vult <ninevult@gmail.com>
2025-09-04 14:53:12 -0400

    Clamp Time to zero

--- Tests/AssCS.Tests/TimeTests.cs
+++ Tests/AssCS.Tests/TimeTests.cs
@@ -7,6 +7,35 @@ namespace AssCS.Tests;
 public class TimeTests
 {
     [Fact]
+    public void From_TimeSpan()
+    {
+        var timeSpan = TimeSpan.FromSeconds(1);
+        var t = new Time(timeSpan);
+
+        t.ShouldBe(Time.FromSeconds(1));
+    }
+
+    [Fact]
+    public void From_TimeSpan_Clamps_To_Zero()
+    {
+        var timeSpan = TimeSpan.FromSeconds(-1);
+        var t = new Time(timeSpan);
+
+        t.ShouldBe(Time.FromSeconds(0));
+    }
+
+    [Fact]
+    public void Operator_Clamps_To_Zero()
+    {
+        var t1 = new Time(TimeSpan.FromSeconds(1));
+        var t2 = new Time(TimeSpan.FromSeconds(2));
+
+        var t3 = t1 - t2;
+
+        t3.ShouldBe(Time.FromSeconds(0));
+    }
+
+    [Fact]
     public void FromAss()
     {
         var t = Time.FromAss("0:02:10.57");
