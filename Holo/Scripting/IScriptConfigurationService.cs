43059aa9e2f2af5ffb86dbf7810f2029b5c1f171
9vult <me@ninevolt.moe>
2025-09-20 11:17:00 -0400

    Add default failure case to ExecutionResult

--- Holo/Scripting/IScriptConfigurationService.cs
+++ Holo/Scripting/IScriptConfigurationService.cs
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MPL-2.0
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Holo.Scripting;
 
 public interface IScriptConfigurationService
 {
-    bool TryGet<T>(IHoloExecutable caller, string key, out T? value);
+    bool TryGet<T>(IHoloExecutable caller, string key, [NotNullWhen(true)] out T? value);
     void Set<T>(IHoloExecutable caller, string key, T value);
     bool Remove(IHoloExecutable caller, string key);
     bool Contains(IHoloExecutable caller, string key);
