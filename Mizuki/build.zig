16ff3ba313dfd57c65184bc5488a9f1c3ee59023
9vult <me@ninevolt.moe>
2025-11-04 09:24:06 -0500

    Update Mizuki build script
    - Fix name capitalization
    - Move away from deprecated functions
    - Make use of 'lib' folder conditional on Windows

--- Mizuki/build.zig
+++ Mizuki/build.zig
@@ -1,12 +1,16 @@
 const std = @import("std");
 
 // Add the required dynamic libraries
-fn linkLibraries(b: *std.Build, obj: *std.Build.Step.Compile) void {
-    obj.addIncludePath(b.path("include"));
-    obj.addLibraryPath(b.path("lib"));
-    obj.linkSystemLibrary("ffms2");
-    obj.linkSystemLibrary("ass");
-    obj.linkLibC();
+fn linkLibraries(b: *std.Build, obj: *std.Build.Step.Compile, target: std.Build.ResolvedTarget) void {
+    obj.root_module.addIncludePath(b.path("include"));
+
+    if (target.result.os.tag == .windows) {
+        obj.root_module.addLibraryPath(b.path("lib"));
+    }
+
+    obj.root_module.linkSystemLibrary("ffms2", .{});
+    obj.root_module.linkSystemLibrary("ass", .{});
+    obj.root_module.link_libc = true;
 }
 
 // Although this function looks imperative, note that its job is to
@@ -43,13 +47,9 @@ pub fn build(b: *std.Build) void {
     // Now, we will create a static library based on the module we created above.
     // This creates a `std.Build.Step.Compile`, which is the build step responsible
     // for actually invoking the compiler.
-    const lib = b.addLibrary(.{
-        .name = "Mizuki",
-        .root_module = lib_mod,
-        .linkage = .dynamic
-    });
+    const lib = b.addLibrary(.{ .name = "mizuki", .root_module = lib_mod, .linkage = .dynamic });
 
-    linkLibraries(b, lib);
+    linkLibraries(b, lib, target);
     lib.root_module.addImport("known-folders", known_folders);
 
     // This declares intent for the library to be installed into the standard
@@ -60,11 +60,11 @@ pub fn build(b: *std.Build) void {
     // This creates another `std.Build.Step.Compile`, but this one builds an executable
     // rather than a static library.
     const exe = b.addExecutable(.{
-        .name = "Mizuki",
+        .name = "mizuki",
         .root_module = lib_mod,
     });
 
-    linkLibraries(b, exe);
+    linkLibraries(b, exe, target);
     exe.root_module.addImport("known-folders", known_folders);
 
     // This declares intent for the executable to be installed into the
