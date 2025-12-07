4b91105fd8f7a67cc2f6b643b72ffae9c89078bc
9vult <me@ninevolt.moe>
2025-12-07 00:58:01 -0500

    Create a config dialog window with nothing in it

---
+++ Ameko/Views/Dialogs/ConfigDialog.axaml.cs
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ameko.Views.Dialogs;
+
+public partial class ConfigDialog : Window
+{
+    public ConfigDialog()
+    {
+        InitializeComponent();
+    }
+}
